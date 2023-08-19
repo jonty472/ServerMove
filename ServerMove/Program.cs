ebf882703c9eb90f134acd1ca103604fa02d6ce0
jonty472 <azttorrs@gmail.com>
2023-08-20 11:43:55 +1200

    Add project files.

---
+++ ServerMove/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ServerMove
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
