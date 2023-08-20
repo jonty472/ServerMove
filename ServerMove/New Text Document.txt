26ddb246d6460d46765b00da199d236ce990deb9
jonty472 <azttorrs@gmail.com>
2023-08-20 12:24:07 +1200

    basic layout of program and testing how to execute a poweshell script

---
+++ ServerMove/New Text Document.txt
@@ -0,0 +1,1 @@
+sqlcmd -S SQLEXPRESS\DESKTOP-7O5A39Q -d MovieDatabase -E
--- ServerMove/Program.cs
+++ ServerMove/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace ServerMove
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ServerMove
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+             * Create menu to manage server move process
+             * menu should include
+             * - backup existing database
+             * - activate full restore on current sql instance
+             * - place transaction logs in destination folder
+             * - restore logs onto future server instance (using NONRECOVERY and when time of move RECOVERY)
+             * - log sucesses and failures
+             * -
+             */
+
+            string scriptPath = "C:\\dev\\csharp\\ServerMove\\ServerMove\\test.ps1";
+
+            Process process = new Process();
+            process.StartInfo.FileName = "powershell.exe";
+            process.StartInfo.Arguments = $"-File \"{scriptPath}\"";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+
+            process.Start();
+
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            Console.WriteLine(output);
+
+
+
         }
     }
 }
